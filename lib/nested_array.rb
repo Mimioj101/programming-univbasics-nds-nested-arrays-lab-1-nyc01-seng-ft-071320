57e7644f6d17a17a05d4c9f7f486761b9b7de227
Mimi Ojserkis <mimioj@gmail.com>
2020-07-01 21:39:26 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -22,7 +22,7 @@ end
 
 def sorted_matrix
  
- produce_storage_room = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
+ produce_storage_room = [CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort]
  
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
